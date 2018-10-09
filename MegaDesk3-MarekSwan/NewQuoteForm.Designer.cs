5a7178b7ea93e8b582319638998fda1fe2f0ecf1
marekswan07 <marekswan07@hotmail.com>
2018-10-09 14:05:56 -0600

    New Quote form modded to reflect the requirements

--- MegaDesk3-MarekSwan/NewQuoteForm.Designer.cs
+++ MegaDesk3-MarekSwan/NewQuoteForm.Designer.cs
@@ -33,42 +33,60 @@
             this.label2 = new System.Windows.Forms.Label();
             this.btnSubmit = new System.Windows.Forms.Button();
             this.lblErrorMessages = new System.Windows.Forms.Label();
+            this.lblCustName = new System.Windows.Forms.Label();
+            this.txtCustName = new System.Windows.Forms.TextBox();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.numericUpDown2 = new System.Windows.Forms.NumericUpDown();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.comboSurface = new System.Windows.Forms.ComboBox();
+            this.numericUpDown3 = new System.Windows.Forms.NumericUpDown();
+            this.comboSpeed = new System.Windows.Forms.ComboBox();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown3)).BeginInit();
             this.SuspendLayout();
             // 
             // btnNewQuoteExit
             // 
             this.btnNewQuoteExit.Font = new System.Drawing.Font("Calibri", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnNewQuoteExit.Location = new System.Drawing.Point(212, 206);
+            this.btnNewQuoteExit.Location = new System.Drawing.Point(312, 292);
             this.btnNewQuoteExit.Name = "btnNewQuoteExit";
             this.btnNewQuoteExit.Size = new System.Drawing.Size(122, 40);
             this.btnNewQuoteExit.TabIndex = 0;
             this.btnNewQuoteExit.Text = "Exit";
             this.btnNewQuoteExit.UseVisualStyleBackColor = true;
+            this.btnNewQuoteExit.Click += new System.EventHandler(this.btnNewQuoteExit_Click);
             // 
             // label1
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Calibri", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(12, 32);
+            this.label1.Location = new System.Drawing.Point(6, 34);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(46, 18);
+            this.label1.Size = new System.Drawing.Size(50, 18);
             this.label1.TabIndex = 1;
-            this.label1.Text = "Width";
+            this.label1.Text = "Width:";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Calibri", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(12, 90);
+            this.label2.Location = new System.Drawing.Point(6, 70);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(46, 18);
+            this.label2.Size = new System.Drawing.Size(50, 18);
             this.label2.TabIndex = 2;
-            this.label2.Text = "Depth";
+            this.label2.Text = "Depth:";
             // 
             // btnSubmit
             // 
             this.btnSubmit.Font = new System.Drawing.Font("Calibri", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnSubmit.Location = new System.Drawing.Point(43, 207);
+            this.btnSubmit.Location = new System.Drawing.Point(89, 292);
             this.btnSubmit.Name = "btnSubmit";
             this.btnSubmit.Size = new System.Drawing.Size(115, 39);
             this.btnSubmit.TabIndex = 3;
@@ -80,25 +98,203 @@
             // 
             this.lblErrorMessages.AutoSize = true;
             this.lblErrorMessages.Font = new System.Drawing.Font("Calibri", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblErrorMessages.Location = new System.Drawing.Point(77, 164);
+            this.lblErrorMessages.Location = new System.Drawing.Point(174, 259);
             this.lblErrorMessages.Name = "lblErrorMessages";
             this.lblErrorMessages.Size = new System.Drawing.Size(187, 18);
             this.lblErrorMessages.TabIndex = 4;
             this.lblErrorMessages.Text = "Will be blank unless showing";
             this.lblErrorMessages.Visible = false;
             // 
+            // lblCustName
+            // 
+            this.lblCustName.AutoSize = true;
+            this.lblCustName.Font = new System.Drawing.Font("Calibri", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblCustName.Location = new System.Drawing.Point(82, 43);
+            this.lblCustName.Name = "lblCustName";
+            this.lblCustName.Size = new System.Drawing.Size(112, 18);
+            this.lblCustName.TabIndex = 5;
+            this.lblCustName.Text = "Customer Name:";
+            // 
+            // txtCustName
+            // 
+            this.txtCustName.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtCustName.Location = new System.Drawing.Point(200, 39);
+            this.txtCustName.Name = "txtCustName";
+            this.txtCustName.Size = new System.Drawing.Size(257, 22);
+            this.txtCustName.TabIndex = 6;
+            this.txtCustName.Validated += new System.EventHandler(this.ValidateCustomerName);
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Font = new System.Drawing.Font("Calibri", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.numericUpDown1.Location = new System.Drawing.Point(90, 32);
+            this.numericUpDown1.Maximum = new decimal(new int[] {
+            96,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Minimum = new decimal(new int[] {
+            24,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(93, 26);
+            this.numericUpDown1.TabIndex = 7;
+            this.numericUpDown1.Value = new decimal(new int[] {
+            24,
+            0,
+            0,
+            0});
+            // 
+            // numericUpDown2
+            // 
+            this.numericUpDown2.Font = new System.Drawing.Font("Calibri", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.numericUpDown2.Location = new System.Drawing.Point(90, 68);
+            this.numericUpDown2.Maximum = new decimal(new int[] {
+            48,
+            0,
+            0,
+            0});
+            this.numericUpDown2.Minimum = new decimal(new int[] {
+            12,
+            0,
+            0,
+            0});
+            this.numericUpDown2.Name = "numericUpDown2";
+            this.numericUpDown2.Size = new System.Drawing.Size(93, 26);
+            this.numericUpDown2.TabIndex = 8;
+            this.numericUpDown2.Value = new decimal(new int[] {
+            12,
+            0,
+            0,
+            0});
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.numericUpDown1);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.numericUpDown2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Font = new System.Drawing.Font("OCR A Extended", 10.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox1.Location = new System.Drawing.Point(21, 91);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(199, 107);
+            this.groupBox1.TabIndex = 9;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Measurements";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Calibri", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(149, 220);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(106, 18);
+            this.label3.TabIndex = 10;
+            this.label3.Text = "Delivery Speed:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Calibri", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(6, 27);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(113, 18);
+            this.label4.TabIndex = 11;
+            this.label4.Text = "Surface Material:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Calibri", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(6, 63);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(120, 18);
+            this.label5.TabIndex = 12;
+            this.label5.Text = "Number of Draws:";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.numericUpDown3);
+            this.groupBox2.Controls.Add(this.comboSurface);
+            this.groupBox2.Controls.Add(this.label4);
+            this.groupBox2.Controls.Add(this.label5);
+            this.groupBox2.Font = new System.Drawing.Font("OCR A Extended", 10.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox2.Location = new System.Drawing.Point(226, 91);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(264, 107);
+            this.groupBox2.TabIndex = 13;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Extras";
+            // 
+            // comboSurface
+            // 
+            this.comboSurface.Font = new System.Drawing.Font("Calibri", 10.25F);
+            this.comboSurface.FormattingEnabled = true;
+            this.comboSurface.Items.AddRange(new object[] {
+            "Oak",
+            "Laminate",
+            "Pine",
+            "Rosewood",
+            "Veneer"});
+            this.comboSurface.Location = new System.Drawing.Point(137, 25);
+            this.comboSurface.Name = "comboSurface";
+            this.comboSurface.Size = new System.Drawing.Size(121, 25);
+            this.comboSurface.TabIndex = 13;
+            this.comboSurface.Text = "Choose an Option";
+            // 
+            // numericUpDown3
+            // 
+            this.numericUpDown3.Font = new System.Drawing.Font("Calibri", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.numericUpDown3.Location = new System.Drawing.Point(138, 61);
+            this.numericUpDown3.Maximum = new decimal(new int[] {
+            7,
+            0,
+            0,
+            0});
+            this.numericUpDown3.Name = "numericUpDown3";
+            this.numericUpDown3.Size = new System.Drawing.Size(93, 26);
+            this.numericUpDown3.TabIndex = 14;
+            // 
+            // comboSpeed
+            // 
+            this.comboSpeed.Font = new System.Drawing.Font("Calibri", 10.75F);
+            this.comboSpeed.FormattingEnabled = true;
+            this.comboSpeed.Items.AddRange(new object[] {
+            "No Rush (14 Days)",
+            "3 Day Rush",
+            "5 Day Rush",
+            "7 Day Rush"});
+            this.comboSpeed.Location = new System.Drawing.Point(261, 218);
+            this.comboSpeed.Name = "comboSpeed";
+            this.comboSpeed.Size = new System.Drawing.Size(121, 25);
+            this.comboSpeed.TabIndex = 14;
+            this.comboSpeed.Text = "Select a Speed";
+            // 
             // NewQuoteForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(370, 283);
+            this.ClientSize = new System.Drawing.Size(545, 378);
+            this.Controls.Add(this.comboSpeed);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.txtCustName);
+            this.Controls.Add(this.lblCustName);
             this.Controls.Add(this.lblErrorMessages);
             this.Controls.Add(this.btnSubmit);
-            this.Controls.Add(this.label2);
-            this.Controls.Add(this.label1);
             this.Controls.Add(this.btnNewQuoteExit);
             this.Name = "NewQuoteForm";
             this.Text = "New Quote";
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown3)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -111,5 +307,17 @@
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Button btnSubmit;
         private System.Windows.Forms.Label lblErrorMessages;
+        private System.Windows.Forms.Label lblCustName;
+        private System.Windows.Forms.TextBox txtCustName;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.NumericUpDown numericUpDown2;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.NumericUpDown numericUpDown3;
+        private System.Windows.Forms.ComboBox comboSurface;
+        private System.Windows.Forms.ComboBox comboSpeed;
     }
 }
--- MegaDesk3-MarekSwan/NewQuoteForm.cs
+++ MegaDesk3-MarekSwan/NewQuoteForm.cs
@@ -20,7 +20,75 @@ namespace MegaDesk3_MarekSwan
 
         private void btnSubmit_Click(object sender, EventArgs e)
         {
+   
+
+            bool validInput = ComboBox_Validating();
+
+            lblErrorMessages.Text = "Please provide surface material and delivery speed";
+            lblErrorMessages.ForeColor = Color.Red;
+            lblErrorMessages.Visible = true;
+
+            if (validInput == true)
+            {
+                lblErrorMessages.Visible = false;
+            }
             
         }
+
+        private void ValidateCustomerName(object sender, EventArgs e)
+        {
+
+            // Confirm that the custName txtbox isn't empty
+
+            if (String.IsNullOrEmpty(txtCustName.Text))
+
+            {
+                lblErrorMessages.Text = "Please enter a customer name";
+                lblErrorMessages.ForeColor = Color.Red;
+                lblErrorMessages.Visible = true;
+                lblCustName.ForeColor = Color.Red;
+                btnSubmit.Enabled = false;
+
+            }
+
+            else
+            {
+                btnSubmit.Enabled = true;
+                lblCustName.ForeColor = Color.Black;
+                lblErrorMessages.Visible = false;
+            }
+        }
+
+        private void btnNewQuoteExit_Click(object sender, EventArgs e)
+        {
+            var mainForm = (MainForm)Tag;
+            mainForm.Show();
+            Close();
+        }
+
+        //this will check if both dropdowns have a selected value before continuing with the submit
+        private bool ComboBox_Validating()
+        {
+            if (comboSpeed.SelectedIndex < 1 || comboSurface.SelectedIndex < 1)
+            {
+                // Cancel the event and select the text to be corrected by the user.
+                if(comboSpeed.SelectedIndex < 1)
+                {
+                    comboSpeed.Select();
+                }
+
+                else
+                {
+                    comboSurface.Select();
+                }
+
+                return false;
+            }
+
+            else
+                return true;
+        }
+
+
     }
 }
